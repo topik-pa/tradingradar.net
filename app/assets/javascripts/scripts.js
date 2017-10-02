f59ec9cd27488010ee8ad33bbd1fa59fffe8145e
Marco Pavan <marcopavan.mp@gmail.com>
2017-10-02 22:46:20 +0200

    I filtri in HP ora funzionano alla grande

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -4,6 +4,97 @@ var tradingRadar = (function() {
 	var $inputElem = $('#pageData');
 
 	var $mainContainer = $('#main-wrapper');
+	
+	
+	var HPFilters = {
+			filter_1: {			        	   
+				index: 0,
+				title: 'Channels break out ',
+				urls: ['/filters/filter_overResistanceBorsaItaliana', '/filters/filter_belowSupportBorsaItaliana'],
+				icon: {
+				    class: 'img-logo-small',
+				    src: '/images/borsa-italiana-logo.png',
+				    title: 'Borsa Italiana',
+				    alt: 'Logo Borsa Italiana'
+				},						
+				captions: ['Resistance', 'Support'],
+				table_headers: [ ['Name', 'Last price', 'Borsa Italiana'], ['Name', 'Last price', 'Borsa Italiana'] ],
+				attributes: [ ['last_price', 'borsa_italiana_resistance'], ['last_price', 'borsa_italiana_support'] ]
+           },
+           filter_2: {			        	   
+				index: 1,
+				title: 'Channels break out ',
+				urls: ['/filters/filter_overResistanceSoleXXIVOre', '/filters/filter_belowSupportSoleXXIVOre'],
+				icon: {
+				    class: 'img-logo-small',
+				    src: '/images/il-sole-24-ore-logo.png',
+				    title: 'Il sole 24 ore',
+				    alt: 'Logo Il sole 24 ore'
+				},						
+				captions: ['Resistance', 'Support'],
+				table_headers: [ ['Name', 'Last price', 'Il sole 24 ore'], ['Name', 'Last price', 'Il sole 24 ore'] ],
+				attributes: [ ['last_price', 'xxivore_resistance'], ['last_price', 'xxivore_support'] ]
+           },
+           filter_3: {			        	   
+				index: 2,
+				title: 'Channels break out ',
+				urls: ['/filters/filter_overResistanceRepubblica', '/filters/filter_belowSupportRepubblica'],
+				icon: {
+				    class: 'img-logo-small',
+				    src: '/images/la-repubblica-logo.png',
+				    title: 'La Repubblica',
+				    alt: 'Logo La Repubblica'
+				},						
+				captions: ['Resistance', 'Support'],
+				table_headers: [ ['Name', 'Last price', 'La Repubblica'], ['Name', 'Last price', 'La Repubblica'] ],
+				attributes: [ ['last_price', 'repubblica_resistance'], ['last_price', 'repubblica_support'] ]
+           },
+           
+           filter_4: {			        	   
+				index: 3,
+				title: 'Suggestions from ',
+				urls: ['/filters/filter_suggestionsMilanoFinanza'],
+				icon: {
+				    class: 'img-logo-small',
+				    src: '/images/milano-finanza-logo.png',
+				    title: 'Milano Finanza',
+				    alt: 'Logo Milano Finanza'
+				},						
+				captions: [],
+				table_headers: [ ['Name', 'Last price', 'MF Risk', 'MF Rating'] ],
+				attributes: [ ['last_price', 'milano_finanza_risk', 'milano_finanza_rating'] ]
+           },
+           
+           filter_5: {			        	   
+				index: 4,
+				title: 'Suggestions from ',
+				urls: ['/filters/filter_suggestionsSoleXXIVOre'],
+				icon: {
+				    class: 'img-logo-small',
+				    src: '/images/il-sole-24-ore-logo.png',
+				    title: 'Il sole 24 ore',
+				    alt: 'Logo Il sole 24 ore'
+				},						
+				captions: [],
+				table_headers: [ ['Name', 'Last price', 'Short Trend', 'FTA Index', 'RSI'] ],
+				attributes: [ ['last_price', 'xxivore_shorttrend', 'xxivore_ftaindex', 'xxivore_rsi'] ]
+          },
+          
+          filter_6: {			        	   
+				index: 5,
+				title: 'Suggestions from ',
+				urls: ['/filters/filter_suggestionsInvestingDotCom'],
+				icon: {
+				    class: 'img-logo-small',
+				    src: '/images/investing-logo.png',
+				    title: 'Investing',
+				    alt: 'Logo Investing'
+				},						
+				captions: [],
+				table_headers: [ ['Name', 'Last price'] ],
+				attributes: [ ['last_price'] ]
+        }
+	}
 
 	return {
 		
@@ -18,6 +109,10 @@ var tradingRadar = (function() {
 		
 		getPageData : function() {
 			return _pageData;
+		},
+		
+		getHPFilters : function() {
+			return HPFilters;
 		},
 
 
@@ -63,9 +158,7 @@ var tradingRadar = (function() {
 		
 		
 		fixHREFs: function() {
-			console.log('Ciccio');
 			var milanoFinanzaUrl = 'https://www.milanofinanza.it';
-			debugger
 		    $links = $mainContainer.find('#milano-finanza a');
 		    $links.each(function() {
 		    	var actualHREF = $(this).attr('href');
@@ -109,182 +202,172 @@ var tradingRadar = (function() {
 				
 			});
 		},
+		
+		
+		highlightStocks : function() {
+			
+			//var totalCalls = 13//$('.filters table').length;
+			//var actualCalls = 0;
+			
+			
+			$(document).on("filtersLoaded", function() {
+				
+				console.log('Inizio io...');
+				
+				//a/ctualCalls++;
+				
+				//if(actualCalls==totalCalls) {
+				
+					$('.suggestions table:first-child tr').each(function() {
+						
+						var isin = $(this).data('isin');
+						var $occurences = $('.suggestions tr[data-isin=' + isin + ']');
+						
+						var count = $occurences.length
+						
+						if(count>=3) {
+							$occurences.addClass('repeated');
+						}
+					});
+					
+					
+					$('.break-outs table.Resistance').eq(0).find('tr').each(function() {
+						
+						var isin = $(this).data('isin');
+						var $occurences = $('.break-outs table.Resistance tr[data-isin=' + isin + ']');
+						
+						var count = $occurences.length
+						
+						if(count>=3) {
+							$occurences.addClass('repeated red');
+						}
+					});
+					
+					
+					$('.break-outs table.Support').eq(0).find('tr').each(function() {
+						
+						var isin = $(this).data('isin');
+						var $occurences = $('.break-outs table.Support tr[data-isin=' + isin + ']');
+						
+						var count = $occurences.length
+						
+						if(count>=3) {
+							$occurences.addClass('repeated green');
+						}
+					});
+				//}
+			});	
+		},
+		
+		
 
 		loadHPFilters : function() {
 			
-			var filters = {
-					filter_1: {			        	   
-						index: 0,
-						title: 'Channels break out ',
-						urls: ['/filters/filter_overResistanceBorsaItaliana', '/filters/filter_belowSupportBorsaItaliana'],
-						icon: {
-						    class: 'img-logo-small',
-						    src: '/images/borsa-italiana-logo.png',
-						    title: 'Borsa Italiana',
-						    alt: 'Logo Borsa Italiana'
-						},						
-						captions: ['Resistance', 'Support'],
-						table_headers: [ ['Name', 'Last price', 'Borsa Italiana'], ['Name', 'Last price', 'Borsa Italiana'] ],
-						attributes: [ ['last_price', 'borsa_italiana_resistance'], ['last_price', 'borsa_italiana_support'] ]
-		           },
-		           filter_2: {			        	   
-						index: 1,
-						title: 'Channels break out ',
-						urls: ['/filters/filter_overResistanceSoleXXIVOre', '/filters/filter_belowSupportSoleXXIVOre'],
-						icon: {
-						    class: 'img-logo-small',
-						    src: '/images/il-sole-24-ore-logo.png',
-						    title: 'Il sole 24 ore',
-						    alt: 'Logo Il sole 24 ore'
-						},						
-						captions: ['Resistance', 'Support'],
-						table_headers: [ ['Name', 'Last price', 'Il sole 24 ore'], ['Name', 'Last price', 'Il sole 24 ore'] ],
-						attributes: [ ['last_price', 'xxivore_resistance'], ['last_price', 'xxivore_support'] ]
-		           },
-		           filter_3: {			        	   
-						index: 2,
-						title: 'Channels break out ',
-						urls: ['/filters/filter_overResistanceRepubblica', '/filters/filter_belowSupportRepubblica'],
-						icon: {
-						    class: 'img-logo-small',
-						    src: '/images/la-repubblica-logo.png',
-						    title: 'La Repubblica',
-						    alt: 'Logo La Repubblica'
-						},						
-						captions: ['Resistance', 'Support'],
-						table_headers: [ ['Name', 'Last price', 'La Repubblica'], ['Name', 'Last price', 'La Repubblica'] ],
-						attributes: [ ['last_price', 'repubblica_resistance'], ['last_price', 'repubblica_support'] ]
-		           },
-		           
-		           filter_4: {			        	   
-						index: 3,
-						title: 'Suggestions from ',
-						urls: ['/filters/filter_suggestionsMilanoFinanza'],
-						icon: {
-						    class: 'img-logo-small',
-						    src: '/images/milano-finanza-logo.png',
-						    title: 'Milano Finanza',
-						    alt: 'Logo Milano Finanza'
-						},						
-						captions: [],
-						table_headers: [ ['Name', 'Last price', 'MF Risk', 'MF Rating'] ],
-						attributes: [ ['last_price', 'milano_finanza_risk', 'milano_finanza_rating'] ]
-		           },
-		           
-		           filter_5: {			        	   
-						index: 4,
-						title: 'Suggestions from ',
-						urls: ['/filters/filter_suggestionsSoleXXIVOre'],
-						icon: {
-						    class: 'img-logo-small',
-						    src: '/images/il-sole-24-ore-logo.png',
-						    title: 'Il sole 24 ore',
-						    alt: 'Logo Il sole 24 ore'
-						},						
-						captions: [],
-						table_headers: [ ['Name', 'Last price', 'Short Trend', 'FTA Index', 'RSI'] ],
-						attributes: [ ['last_price', 'xxivore_shorttrend', 'xxivore_ftaindex', 'xxivore_rsi'] ]
-		          },
-		          
-		          filter_6: {			        	   
-						index: 5,
-						title: 'Suggestions from ',
-						urls: ['/filters/filter_suggestionsInvestingDotCom'],
-						icon: {
-						    class: 'img-logo-small',
-						    src: '/images/investing-logo.png',
-						    title: 'Investing',
-						    alt: 'Logo Investing'
-						},						
-						captions: [],
-						table_headers: [ ['Name', 'Last price'] ],
-						attributes: [ ['last_price'] ]
-		        }
-			}
-	           
-          
+			var filters = tradingRadar.getHPFilters();
 			
+			var filtersCount = Object.keys(filters).length;
+			var filtersWorked = 0;
 			
-			for (var filter in filters) {
-			    if (filters.hasOwnProperty(filter)) {
-			    	
-			    	var obj = filters[filter];
-
-			    	var $filter = $mainContainer.find('.filters .hp-filter:eq(' + obj.index + ')');
-			    	var $titleElem = $filter.find('h2');
-			    	var $contentElem = $filter.find('.x_content');
-			    	var count = 0;
-			    	
-			    	$titleElem.html(obj.title);
-			    	
-			    	var $ico = $('<img/>', obj.icon).appendTo($titleElem);
-
-		    	
-			    	obj.urls.forEach(function(url, index) {
-			    		
-			    		var $table = $('<table/>', {
-						    class: 'tablesorter ' + obj.captions[index]
-						}).appendTo($contentElem);
-			    		
-			    		var $caption = $('<caption/>', {
-						    text: obj.captions[index]
-						});
-				    	$table.append($caption);
+			var filtersPromise = new Promise(function(resolve, reject) {
+				for (var filter in filters) {
+				    if (filters.hasOwnProperty(filter)) {
 				    	
-				    	var thead = '';
-						thead = '<thead><tr>';
-						obj.table_headers[index].forEach(function(theader) {
-							thead += '<th>' + theader + '</th>';
-						});
-						thead += '</tr></thead>';
-				        $table.append(thead);
-				        
-				        
-				        
-			    		$.ajax({
-							url : url,
-							data : 'text/json',
-							type : 'get',
-							success : function(response) {				
+				    	
+				    	var filterPromise = new Promise(function(resolve, reject) {
+				    		var obj = filters[filter];
 
-								var tbody = '<tbody>';
-						        $.each(response, function (i, item) {
-						        	var rowClass = i%2===0 ? 'even' : 'odd';
-						        	
-						        	tbody += '<tr class="' + rowClass + '" data-isin="' + item.isin + '">';
-						        	tbody += '<td><a href="' + '/stock_page/index?isin=' + item.isin + '">' + item.name + '</a></td>';
-
-						        	obj.attributes[index].forEach(function(attribute) {
-										
-										tbody += '<td>' + item[attribute] + '</td>';
-									});
-									tbody += '</tr>';
-									tbody += '</tbody>';
-						        	
-						        });
+					    	var $filter = $mainContainer.find('.filters .hp-filter:eq(' + obj.index + ')');
+					    	var $titleElem = $filter.find('h2');
+					    	var $contentElem = $filter.find('.x_content');
+					    	var count = 0;
+					    	
+					    	$titleElem.html(obj.title);
+					    	
+					    	var $ico = $('<img/>', obj.icon).appendTo($titleElem);
+					    					    
+					    	
+					    	obj.urls.forEach(function(url, index) {
+					    		
+					    		var $table = $('<table/>', {
+								    class: 'tablesorter ' + obj.captions[index]
+								}).appendTo($contentElem);
+					    		
+					    		var $caption = $('<caption/>', {
+								    text: obj.captions[index]
+								});
+						    	$table.append($caption);
+						    	
+						    	var thead = '';
+								thead = '<thead><tr>';
+								obj.table_headers[index].forEach(function(theader) {
+									thead += '<th>' + theader + '</th>';
+								});
+								thead += '</tr></thead>';
+						        $table.append(thead);					        				       
 						        
-						        $table.append(tbody);
-								
-							},
-							error : function() {
-								console.log('error');
-							},
-							complete : function() {
-								count++;
-								if(count === obj.urls.length) {
-									$(document).trigger("filterComplete", [obj.index]);
-									$contentElem.addClass('loaded');
-								}
-								
-							}
-						});
-			    		
-			    	});
-			    	
-			    }
-			}
+						        $.ajax({
+									url : url,
+									data : 'text/json',
+									type : 'get',
+									success : function(response) {				
+
+										var tbody = '<tbody>';
+								        $.each(response, function (i, item) {
+								        	var rowClass = i%2===0 ? 'even' : 'odd';
+								        	
+								        	tbody += '<tr class="' + rowClass + '" data-isin="' + item.isin + '">';
+								        	tbody += '<td><a href="' + '/stock_page/index?isin=' + item.isin + '">' + item.name + '</a></td>';
+
+								        	obj.attributes[index].forEach(function(attribute) {
+												
+												tbody += '<td>' + item[attribute] + '</td>';
+											});
+											tbody += '</tr>';
+											tbody += '</tbody>';
+								        	
+								        });
+								        
+								        $table.append(tbody);
+										
+									},
+									error : function(err) {
+										console.error(err);	
+										
+									},
+									complete : function() {
+										count++;
+										if(count === obj.urls.length) {
+											$contentElem.addClass('loaded');	
+											filtersWorked++;
+											if (filtersCount == filtersWorked) {
+												resolve();
+											}
+										}										
+									}
+								});
+					    		
+					    	});
+					    	
+					    	
+					    	
+					    	
+			    		}).then(function() {
+			    			resolve();
+			    		}).catch(function(err) {
+			    			console.error(err);
+			    			reject();
+			    		});
+				    	
+				    }
+				}
+			}).then(function() {
+				$(document).trigger("filtersLoaded");
+			}).catch(function(err) {
+    			console.error(err);
+    		});
+
 			
-			
+					
 			
 		}
 
@@ -323,7 +406,7 @@ $(document).ready(function() {
 	
 	// setTimeout(myApp.highlightStocks(), 3000);
 	
-	// tradingRadar.highlightStocks();
+	
 	
 	
 	
@@ -335,7 +418,8 @@ $(document).ready(function() {
 	
 	
 	if($('.welcome').length) {
-		//tradingRadar.loadHPFilters();
+		tradingRadar.loadHPFilters();
+		tradingRadar.highlightStocks();
 	}
 	
 
