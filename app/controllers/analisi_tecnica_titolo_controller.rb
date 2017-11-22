4d20c003980c6bc567851ca35be2073c8477b875
Marco Pavan <marcopavan.mp@gmail.com>
2017-11-22 22:49:10 +0100

    Non t rovava l isin

--- app/controllers/analisi_tecnica_titolo_controller.rb
+++ app/controllers/analisi_tecnica_titolo_controller.rb
@@ -7,30 +7,30 @@ class AnalisiTecnicaTitoloController < ApplicationController
     def index
       @user = User.find_by_email($user_email)
               
-      isin = params[:isin] ? params[:isin] : ''     
-      @stock = Stock.find_by isin: isin  
-      @urls = Url.find_by isin: isin
+      @isin = params[:isin] ? params[:isin] : ''     
+      @stock = Stock.find_by isin: @isin  
+      @urls = Url.find_by isin: @isin
       
-      @analisis = Analysis.find_by isin: isin  
+      @analisis = Analysis.find_by isin: @isin  
       @studiesSize = @stock.studies.size 
       @lastStudy = @stock.studies.last
       
       #Chart image
-      @urlchartImage = 'http://indici.soldionline.it/ComboChart.aspx?Codice=' + isin + '&TimeRange=360&ChartSize=M&Volume=1&VGrid=1&HGrid=1&ChartType=0&Band=-1&avgType1=N&movAvg1=10&avgType2=N&movAvg2=25&Indicator1=CCI&Indicator2=None&Indicator3=None&Indicator4=None&MID=&SymbolName=' + @stock.name + '&TenYears=0'
+      @urlchartImage = 'http://indici.soldionline.it/ComboChart.aspx?Codice=' + @isin + '&TimeRange=360&ChartSize=M&Volume=1&VGrid=1&HGrid=1&ChartType=0&Band=-1&avgType1=N&movAvg1=10&avgType2=N&movAvg2=25&Indicator1=CCI&Indicator2=None&Indicator3=None&Indicator4=None&MID=&SymbolName=' + @stock.name + '&TenYears=0'
             
-      @urlBorsaItaliana = @urls.url1
-      @urlBorsaItaliana2 = @urls.url2   
+      #@urlBorsaItaliana = @urls.url1
+      #@urlBorsaItaliana2 = @urls.url2   
       
-      @urlSole24Ore = @urls.url3 
+      #@urlSole24Ore = @urls.url3 
       
-      @urlLaRepubblica = @urls.url6 
+      #@urlLaRepubblica = @urls.url6 
       
-      @urlInvesting = @urls.url7 
+      #@urlInvesting = @urls.url7 
       
-      @urlMilanoFinanza = @urls.url9
-      @urlMilanoFinanza2 = @urls.url10
+      #@urlMilanoFinanza = @urls.url9
+      #@urlMilanoFinanza2 = @urls.url10
       
-      @urlSoldiOnLine = @urls.url5
+      #@urlSoldiOnLine = @urls.url5
 
     end
     
